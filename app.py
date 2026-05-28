9735eb92fe1489a5ca2c9c7a50bc33da61ae0c76
poornima <poornimapatcharu@gmail.com>
2026-05-29 00:35:28 +1000

    changes in port

--- app.py
+++ app.py
@@ -9,5 +9,5 @@ def hello():
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 7777))
+    port = int(os.environ.get("PORT", 8888))
     app.run(debug=True,host='0.0.0.0',port=port)
